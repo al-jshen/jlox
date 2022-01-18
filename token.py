86961802ce75926b2591a524062bacca2ee4d2e5
Jeff Shen <jshen2014@hotmail.com>
2022-01-18 00:43:53 -0500

    4.2

--- token.py
+++ token.py
@@ -1,2 +1,12 @@
+from token_type import TokenType
+from typing import Any
+
 class Token:
-    pass
+    def __init__(self, type: TokenType, lexeme: str, literal: Any, line: int):
+        self.type = type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+
+    def __str__(self):
+        return f'{self.type} {self.lexeme} {self.literal}'
