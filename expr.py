10ae0350ef9bd7c9ea6d77da2b9fc876838f6b7d
Jeff Shen <jshen2014@hotmail.com>
2022-07-29 22:12:01 -0700

    ?????

--- expr.py
+++ expr.py
@@ -1,28 +1,32 @@
 from typing import Any
 from tokens import Token
-
 class Expr:
   pass
 
 class Binary(Expr):
   def __init__(self, left: Expr, operator: Token, right: Expr):
     self.left: Expr = left
-
     self.operator: Token = operator
-
     self.right: Expr = right
+  def __str__(self):
+    f"{self.left} {self.operator} {self.right}"
 
 class Grouping(Expr):
   def __init__(self, expression: Expr):
     self.expression: Expr = expression
+  def __str__(self):
+    f"{self.expression}"
 
 class Literal(Expr):
   def __init__(self, value: Any):
     self.value: Any = value
+  def __str__(self):
+    f"{self.value}"
 
 class Unary(Expr):
   def __init__(self, operator: Token, right: Expr):
     self.operator: Token = operator
-
     self.right: Expr = right
+  def __str__(self):
+    f"{self.operator} {self.right}"
 
--- generate_ast.py
+++ generate_ast.py
@@ -1,25 +1,38 @@
 import sys
-from typing import List
+from typing import List, Dict
 
 
-def generate_ast(output_dir: str, base_name: str, types: List[List[str]]):
+def generate_ast(
+    output_dir: str,
+    base_name: str,
+    types: Dict[str, str],
+    imports: Dict[str, str] = None,
+    string_reps: bool = False,
+):
 
     code: List[str] = []
 
-    code.append("from typing import Any\n")
-    code.append("from tokens import Token\n\n")
+    if imports:
+        for lib, vals in imports.items():
+            code.append(f"from {lib} import {vals}\n")
 
     code.append(f"class {base_name}:\n  pass\n\n")
 
-    for t in types:
-        class_name = t[0]
-        fields = t[1]
+    for class_name, fields in types.items():
 
         code.append(f"class {class_name}({base_name}):\n")
         code.append(f"  def __init__(self, {fields}):\n")
         for field in fields.split(", "):
             field_name, field_type = field.split(": ")
-            code.append(f"    self.{field_name}: {field_type} = {field_name}\n\n")
+            code.append(f"    self.{field_name}: {field_type} = {field_name}\n")
+
+        if string_reps:
+            code.append(f"  def __str__(self):\n")
+            str_rep = "".join(
+                [" {self." + i.split(": ")[0] + "}" for i in fields.split(", ")]
+            )
+            code.append(f'    f"{str_rep.strip()}"\n')
+        code.append("\n")
 
     with open(f"{output_dir}/{base_name.lower()}.py", "w") as f:
         f.writelines(code)
@@ -35,10 +48,12 @@ if __name__ == "__main__":
     generate_ast(
         output_dir,
         "Expr",
-        [
-            ["Binary", "left: Expr, operator: Token, right: Expr"],
-            ["Grouping", "expression: Expr"],
-            ["Literal", "value: Any"],
-            ["Unary", "operator: Token, right: Expr"],
-        ],
+        {
+            "Binary": "left: Expr, operator: Token, right: Expr",
+            "Grouping": "expression: Expr",
+            "Literal": "value: Any",
+            "Unary": "operator: Token, right: Expr",
+        },
+        {"typing": "Any", "tokens": "Token"},
+        string_reps=True,
     )
